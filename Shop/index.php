3cf233d6ba371ef6f330f37def404cf48c65fd71
rsdawa <zyk.happy@gmail.com>
2017-09-06 19:18:07 +0800

    已有模板引入框架;定义路径常量

--- Shop/index.php
+++ Shop/index.php
@@ -2,5 +2,9 @@
 header("content-type:text/html;charset=utf-8");
 //开启调试模式
 define('APP_DEBUG', true);
+//静资资源路径常量
+define('CSS_URL', '/Shop/Home/Public/css/');
+define('IMG_URL', '/Shop/Home/Public/images/');
+define('JS_URL', '/Shop/Home/Public/js/');
 //加载入口文件
 require '../ThinkPHP/ThinkPHP.php';
